ff2ef5b7c704e9bdc8025365c3562dccc467b3ef
Divya <divya.amirtharaj@gmail.com>
2023-04-25 14:26:34 -0400

    smth broke

--- nba_data_scrape.py
+++ nba_data_scrape.py
@@ -26,93 +26,103 @@ def get_basketball_reference(player, urls, start):
         column_names_raw=[head.text for item in head][0]
         column_names_polished=column_names_raw.replace("\n",",").split(",")[2:-1]
 
-        """Extracting full list of player_data"""
-        players=[]
-        for i in range(len(table)):
-            for td in table[i].find_all("td"):
-                if td.text == player:
-                    for td in table[i].find_all("td"):
-                        players.append(td.text)
-            
-        """Creating Player Dictionary"""     
-        player_dict={}
-        Player=[players[0]]
-        Pos=[players[1]]
-        Age=[players[2]]
-        Tm=[players[3]]
-        G=[int(players[4])] 
-        GS=[int(players[5])] 
-        MP=[float(players[6])]
-        FG=[float(players[7])]
-        FGA=[float(players[8])]
-        FG_perc=[players[9]]
-        Three_point=[float(players[10])]
-        Three_point_A=[float(players[11])]
-        Three_point_perc=[players[12]]
-        Two_point=[float(players[13])] 
-        Two_point_A=[float(players[14])] 
-        Two_point_perc=[players[15]]
-        eFG_perc=[players[16]]
-        FT=[float(players[17])]
-        FTA=[float(players[18])]
-        FT_perc=[players[19]]
-        ORB=[float(players[20])]
-        DRB=[float(players[21])]
-        TRB=[float(players[22])] 
-        AST=[float(players[23])] 
-        STL=[float(players[24])]
-        BLK=[float(players[25])]
-        TOV=[float(players[26])]
-        PF=[float(players[27])]
-        PPG=[float(players[28])]
+        try:
+            """Extracting full list of player_data"""
+            players=[]
+            for i in range(len(table)):
+                for td in table[i].find_all("td"):
+                    if td.text == player:
+                        for td in table[i].find_all("td"):
+                            players.append(td.text)
+        except Exception as e:
+            print(e)
         
-        player_dict={
-                    "Player":Player,
-                    "Pos":Pos,
-                    "Age":Age,
-                    "Tm":Tm,
-                    "G":G,
-                    "MP":MP,
-                    "FG":FG,
-                    "FGA":FGA,
-                    "FG%":FG_perc,
-                    "3P":Three_point,
-                    "3PA":Three_point_A,
-                    "3P%":Three_point_perc,
-                    "2P":Two_point,
-                    "2PA":Two_point_A,
-                    "2P%":Two_point_perc,
-                    "eFG%":eFG_perc,
-                    "FT":FT,
-                    "FTA":FTA,
-                    "FT%":FT_perc,
-                    "ORB":ORB,
-                    "DRB":DRB,
-                    "TRB":TRB,
-                    "AST":AST,
-                    "STL":STL,
-                    "BLK":BLK,
-                    "TOV":TOV,
-                    "PF":PF,
-                    "PPG":PPG}
-        #print(player_dict)
-        full_history[start] = player_dict
+        player_dict={}
+        if len(players) > 0:
+            """Creating Player Dictionary"""     
+            Player=players[0]
+            Pos=players[1]
+            Age=players[2]
+            Tm=players[3]
+            G=int(players[4])
+            GS=int(players[5]) 
+            MP=float(players[6])
+            FG=float(players[7])
+            FGA=float(players[8])
+            FG_perc=players[9]
+            Three_point=float(players[10])
+            Three_point_A=float(players[11])
+            Three_point_perc=players[12]
+            Two_point=float(players[13])
+            Two_point_A=float(players[14])
+            Two_point_perc=players[15]
+            eFG_perc=players[16]
+            FT=float(players[17])
+            FTA=float(players[18])
+            FT_perc=players[19]
+            ORB=float(players[20])
+            DRB=float(players[21])
+            TRB=float(players[22])
+            AST=float(players[23]) 
+            STL=float(players[24])
+            BLK=float(players[25])
+            TOV=float(players[26])
+            PF=float(players[27])
+            PPG=float(players[28])
+            
+            player_dict={
+                        "Player":Player,
+                        "Pos":Pos,
+                        "Age":Age,
+                        "Tm":Tm,
+                        "G":G,
+                        "MP":MP,
+                        "FG":FG,
+                        "FGA":FGA,
+                        "FG%":FG_perc,
+                        "3P":Three_point,
+                        "3PA":Three_point_A,
+                        "3P%":Three_point_perc,
+                        "2P":Two_point,
+                        "2PA":Two_point_A,
+                        "2P%":Two_point_perc,
+                        "eFG%":eFG_perc,
+                        "FT":FT,
+                        "FTA":FTA,
+                        "FT%":FT_perc,
+                        "ORB":ORB,
+                        "DRB":DRB,
+                        "TRB":TRB,
+                        "AST":AST,
+                        "STL":STL,
+                        "BLK":BLK,
+                        "TOV":TOV,
+                        "PF":PF,
+                        "PPG":PPG}
+            #print(player_dict)
+        if player_dict:
+            full_history[start] = player_dict
         start += 1
-    print(full_history)
+    #print(full_history)
     return full_history
 
-def convert_to_panda(full_history):
+def convert_to_panda(full_history, name):
     df = pd.DataFrame.from_dict(full_history, orient='index')
     df = df.reset_index().rename(columns={'index': 'Year'})
     df = df[['Year', 'Player', 'Pos', 'Age', 'Tm', 'G', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PPG']]
     print(df)
-    df.to_csv('example.csv', index=False)
+    df.to_csv(f"{name}.csv", index=True)
 
 
-#name = input("For which basketball player would you like to get updated stats?: ")
-#start_year = input("Starting in what year? ")
-#end_year = input("Ending in what year? ")
-urls = get_url(int(2019), int(2022))
+def run(player, start, end):
+    urls = get_url(start, end)
+    data = get_basketball_reference(player, urls, start)
+    convert_to_panda(data, player)
 
-data = get_basketball_reference("Stephen Curry", urls, 2019)
-convert_to_panda(data)
+run("Rashad McCants", 2005, 2009)
+run("Lamar Odom", 1999, 2013)
+run("Kris Humphries", 2004, 2017)
+run("James Harden", 2009, 2023)
+run("Tristan Thompson", 2011, 2023)
+run("Blake Griffin", 2009, 2023)
+run("Ben Simmons", 2016, 2023)
--- notebook.md
+++ notebook.md
@@ -1,14 +1,22 @@
 ## Dating History Timeline
 
 ### Confirmed Relationships
-Rashad McCants (Khloe Kardashian) 2008 (dated) </br>
+Rashad McCants (Khloe Kardashian) 2008 (dated) </br> 
+- Career: 2005-2009
 Lamar Odom (Khloe Kardashian) 08/2009 - 2014 (married) </br>
+- Career: 1999-2013
 Kris Humphries (Kim Kardashian) 12/2010 - 10/2011 (married) </br>
+- Career: 2004-2017
 James Harden (Khloe Kardashian) 06/2015 - 02/2016 (dated) </br>
+- Career: 2009-current
 Tristan Thompson (Khloe Kardashian) 06/2016 - current (on and off, two children) </br>
+- Career: 2011-current
 Blake Griffin (Kendall Jenner) 06/2017 - 02/2018 (dated) </br>
+- Career: 2009-current
 Ben Simmons (Kendall Jenner) 06/2018 - 05/2019 (dated) </br>
+- Career: 2016-current
 Devin Booker (Kendall Jenner) 04/2020 - 11/2022 (dated) </br>
+- Career: 2015-current
 
 ### Rumored (but probably friends)
 D'Angelo Russell (Kendall Jenner) 2015 </br>
